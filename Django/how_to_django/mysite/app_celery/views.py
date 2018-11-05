311ef36558557e3c9b8d961ec42d582b10ccec53
greenfrog <greenfrog82@naver.com>
2018-11-06 01:32:24 +0900

    Python How to Django - test runserver multiple requests

--- Django/how_to_django/mysite/app_celery/views.py
+++ Django/how_to_django/mysite/app_celery/views.py
@@ -1,3 +1,6 @@
+import time
+import threading
+
 from django.http import JsonResponse
 
 from app_model.models import Article
@@ -5,9 +8,20 @@ from .tasks import create_random_user_accounts
 
 
 def perform_celery_task(request):
-    ret = create_random_user_accounts.delay(100)
+    # ret = create_random_user_accounts.delay(100)
+    # if request.GET.get('test'):
+    #     import pdb; pdb.set_trace()
+    #     time.sleep(5)
+
+    print('----> {}'.format(threading.get_ident()))
     
+    if request.GET.get('test'):
+        for i in range(10000000):
+            for j in range(100000000):
+                pass
+        
     return JsonResponse({
         'result':'success',
-        'id': id(Article.objects.first()),
+        # 'id': id(Article.objects.first()),
+        'id': threading.get_ident(),
     })
