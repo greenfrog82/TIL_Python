f3ddf87d3f1b9757ceea18a6f5470c36acdd4e23
greenfrog82 <greenfrog82@naver.com>
2018-11-16 16:05:34 +0900

    Python asyncio

---
+++ Basic/async/readme.md
@@ -0,0 +1,6 @@
+# asyncio 사용하기
+
+
+# Reference
+
+* [48.8 asyncio 사용하기](https://dojang.io/mod/page/view.php?id=1167)
---
+++ Basic/async/src/ex_sync.py
@@ -0,0 +1,18 @@
+from time import time
+from urllib.request import Request, urlopen
+ 
+urls = ['https://www.google.co.kr/search?q=' + i
+        for i in ['apple', 'pear', 'grape', 'pineapple', 'orange', 'strawberry']]
+ 
+begin = time()
+result = []
+for idx, url in enumerate(urls):
+    request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})    # UA가 없으면 403 에러 발생
+    response = urlopen(request)
+    page = response.read()
+    result.append(len(page))
+    print('[{}] {}'.format(idx, url))
+ 
+print(result)
+end = time()
+print('실행 시간: {0:.3f}초'.format(end - begin))
