dc2cb8fc1f2269a6f75d79fedb77ed653348900a
greenfrog82 <greenfrog82@naver.com>
2018-11-30 08:09:40 +0900

    Django - about TransactionTestCase

--- Django/django_study/mysite/my_app/urls.py
+++ Django/django_study/mysite/my_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    # path(r'get-url/', views.get_url, name='get-url'),
-    re_path(r'get-url/', 'my_app.views.get_url', name='get-url'),
+    path(r'get-url/', views.get_url, name='get-url'),
+    # re_path(r'get-url/', 'my_app.views.get_url', name='get-url'),
 ]
